4a8894d35d57a2024623c916875794d093dc976c
Ricardo Magno <RicardoMagno>
2017-04-06 12:55:21 -0300

    problema com scipy

--- lbp/codigo.py
+++ lbp/codigo.py
@@ -3,108 +3,100 @@ import scipy
 import scipy.misc
 import pdb
 import numpy as np
-import math 
+import math
+
+class LBPError(Exception): pass
+class LBPOutOfRangeError(LBPError): pass
 
 class LBP:
-	
-	def __init__(self):
-		pass
 
-	# metodo para calcular a coordenada x
-	def calculates_x_coordinate(self, xc, r, p, P):
-		cos = math.cos(math.pi*((2*p)/P))
-		if (cos>0): cos = math.ceil(cos)
-		elif(cos<0): cos = math.floor(cos)
-		return (xc + (r*cos))
-	
-	# metodo para calcular a coordenada y		
-	def calculates_y_coordinate(self, yc, r, p, P):
-		sin = math.sin(math.pi*((2*p)/P))
-		if (sin>0): sin = math.ceil(sin)
-		elif(sin<0): sin = math.floor(sin)
+    def __init__(self):
+        pass
 
-		result = yc + (r*sin)
-		return result
+    # metodo para calcular a coordenada x
+    def calculates_x_coordinate(self, xc, r, p, P):
+        cos = round(math.cos((math.pi/P)*(2*p)))
+        return xc + (r*cos)
 
-	# metodo para converter o numero binario para decimal		
-	def calculates_binary(self, matVals):
-		num = 0
-		binary = 2
-		for i in range(len(matVals[0])):
-			if matVals[0][i] == 1:
-				#transformacao generica
-				num += binary**(len(matVals[0])-i-1)
-		return num
-	
-	def computeLBP(self, imgIn, p, r):
-	
-		matVals = np.zeros((1, p))
-		matLbp = np.zeros((len(imgIn), len(imgIn[0])))
-		
-		for row in range(r, len(imgIn)-r):
-			for col in range(r, len(imgIn[row])-r):
-				centerValue = int(imgIn[row,col]) 
+    # metodo para calcular a coordenada y
+    def calculates_y_coordinate(self, yc, r, p, P):
+        sin = round(math.sin((math.pi/P)*(2*p)))
+        return yc - (r*sin)
 
-				for i in range(p):
+    # metodo para converter o numero binario para decimal
+    def calculates_binary(self, matVals):
+        num = 0
+        binary = 2
+        for i in range(len(matVals[0])):
+            if matVals[0][i] == 1:
+                #transformacao generica
+                num += binary**(len(matVals[0])-i-1)
+        return num
 
-					if int(imgIn[self.calculates_x_coordinate(row, r, i, p) ,self.calculates_y_coordinate(col, r, i, p)]) > centerValue:
-						matVals[0, i] = 1
-				dec = self.calculates_binary(matVals)
-				matLbp[row,col] = dec
+    def computeLBP(self, imgIn, p, r):
+        if not (p<=(r*8)): raise LBPOutOfRangeError, 'p maior que valor possivel'
+        matVals = np.zeros((1, p))
+        matLbp = np.zeros((len(imgIn), len(imgIn[0])))
+        for row in range(r, len(imgIn)-r):
+            for col in range(r, len(imgIn[row])-r):
+                centerValue = int(imgIn[row,col])
+                for i in range(p):
+                    if int(imgIn[self.calculates_x_coordinate(row, r, i, p) ,self.calculates_y_coordinate(col, r, i, p)]) > centerValue:
+                        matVals[0, i] = 1
+                    dec = self.calculates_binary(matVals)
+                matLbp[row,col] = dec
+        return matLbp
 
-		return matLbp
+    def getHistogram(self, imgIn):
 
-				
-	def getHistogram(self, imgIn):
+        hist = np.zeros((1, 256))
 
-		hist = np.zeros((1, 256))
-	
-		for row in range(0, len(imgIn)):
-			for col in range(0, len(imgIn[row])):
-				value = imgIn[row,col]
-				#pdb.set_trace()
-				hist[0][int(value)] = hist[0][int(value)] + 1
-		return hist
+        for row in range(0, len(imgIn)):
+            for col in range(0, len(imgIn[row])):
+                value = imgIn[row,col]
+                #pdb.set_trace()
+                hist[0][int(value)] = hist[0][int(value)] + 1
+        return hist
 
-	def equalizeHistogram(self, imgIn):
-	
-	#	pdb.set_trace()	
-		
-		quantRows = len(imgIn)
-		quantCols = len(imgIn[0])
-	
-		imgHE = np.zeros((quantRows, quantCols))
-	
-		hist = self.getHistogram(imgIn)
-		minValHist = np.amin(hist)
-	
-		for row in range(0, len(imgIn)):
-			for col in range(0, len(imgIn[row])):
-				#imgHE[row, col] = imgIn[row,col]/(quantRows*quantCols)
-				value = imgIn[row,col]
-				imgHE[row, col] = ((hist[0,int(value)] - minValHist)/(quantRows*quantCols-1))*255
-			#print imgHE[row, col]
-	
-		return imgHE
+    def equalizeHistogram(self, imgIn):
+
+    #	pdb.set_trace()
+
+        quantRows = len(imgIn)
+        quantCols = len(imgIn[0])
+
+        imgHE = np.zeros((quantRows, quantCols))
+
+        hist = self.getHistogram(imgIn)
+        minValHist = np.amin(hist)
+
+        for row in range(0, len(imgIn)):
+            for col in range(0, len(imgIn[row])):
+                #imgHE[row, col] = imgIn[row,col]/(quantRows*quantCols)
+                value = imgIn[row,col]
+                imgHE[row, col] = ((hist[0,int(value)] - minValHist)/(quantRows*quantCols-1))*255
+            #print imgHE[row, col]
+
+        return imgHE
 
 
 lbp = LBP()
 
 
 if __name__=="__main__":
-	imgName = '/home/mainara/projeto_eeg/jaffeimages/jaffe/KA.AN1.39.tiff'
+    imgName = '/home/ricardo/Documentos/testarAoRedor.xcf'
 
-	img1 = scipy.misc.imread(imgName, flatten=True)
+    img1 = scipy.misc.imread(imgName, flatten=True)
     #scipy.misc.imshow(img1)
 
-	lbp.computeLBP(img1, 16, 2)
+    lbp.computeLBP(img1, 16, 2)
 
-	scipy.misc.imsave('imgSaida2.jpg', img1)
+    scipy.misc.imsave('imgSaida2.jpg', img1)
 
-	imgHE = lbp.equalizeHistogram(img1)
+    imgHE = lbp.equalizeHistogram(img1)
 
-	scipy.misc.imshow(imgHE)
+    scipy.misc.imshow(imgHE)
 
-	imgResized = scipy.misc.imresize(imgHE, (64, 64))
-	scipy.misc.imsave('imgResized.jpg', imgResized)
+    imgResized = scipy.misc.imresize(imgHE, (64, 64))
+    scipy.misc.imsave('imgResized.jpg', imgResized)
 
--- lbp/ut_codigo.py
+++ lbp/ut_codigo.py
@@ -6,117 +6,127 @@ import scipy
 from codigo import LBP
 
 class UT_LBP(unittest.TestCase):
-	
-	valores_p = {4,8,16}
-	valores_r = {1,2,3}
-	######################################
-	def setUp(self):
-		pass
-	
-	######################################
-	'''
-	def test_computeLBP(self):
-	
-		lbp = LBP()
-		
-		imgName = 'imgTstLbp.png'
-		img1 = scipy.misc.imread(imgName, flatten=True)
-		
-		matLbp = lbp.computeLBP(img1, 4, 1)
-		
-		self.assertEqual(matLbp[0,0], -1)
-	'''
-	def test_calculates_x_coordinate(self):
-		lbp = LBP()
-		xc = 2
-		r = 1
-		pixel = 1
-		P = 4
-		x1 = xc + 1
-		x2 = xc
-		x3 = xc - 1
-		x4 = xc
-		x_viz1 = lbp.calculates_x_coordinate(xc, r, 4, P)
-		x_viz2 = lbp.calculates_x_coordinate(xc, r, 3, P)
-		x_viz3 = lbp.calculates_x_coordinate(xc, r, 2, P)
-		x_viz4 = lbp.calculates_x_coordinate(xc, r, 1, P)
 
-		self.assertEqual(x1, x_viz1)
-		self.assertEqual(x2, x_viz2)
-		self.assertEqual(x3, x_viz3)
-		self.assertEqual(x4, x_viz4)
+    valores_p = {4,8,16}
+    valores_r = {1,2,3}
+    ######################################
+    def setUp(self):
+        pass
 
-	def testAutoValueX(self):
-		lbp = LBP()
-		xc = 2
-		for P in (4,8,16):
-			for r in (1,2,3):
-				x = np.zeros((1, P))
-				x[0] = xc + r
-				x[0][(P/4):P*3/4] = xc - r
-				x[0][(P/4)] = xc
-				x[0][(P*3/4)] = xc
-				for i in range(P-1,-1,-1):
-					if(x[0][i]!=lbp.calculates_x_coordinate(xc, r, i, P)):
-						print " ",xc,r,i,P
-					self.assertEqual(x[0][i], lbp.calculates_x_coordinate(xc, r, i+1, P))
-	
-	def test_calculates_y_coordinate(self):
-		lbp = LBP()
-		yc = 2
-		r = 1
-		pixel = 1
-		P = 4
-		
-		y1 = yc
-		y2 = yc - 1
-		y3 = yc
-		y4 = yc + 1
-		
-		y_viz1 = lbp.calculates_y_coordinate(yc, r, 4, P)
-		y_viz2 = lbp.calculates_y_coordinate(yc, r, 3, P)
-		y_viz3 = lbp.calculates_y_coordinate(yc, r, 2, P)
-		y_viz4 = lbp.calculates_y_coordinate(yc, r, 1, P)
-		
-		self.assertEqual(y1, y_viz1)
-		self.assertEqual(y2, y_viz2)
-		self.assertEqual(y3, y_viz3)
-		self.assertEqual(y4, y_viz4)
-		
-	def testAutoValueY(self):
-		lbp = LBP()
-		yc = 2
-		for P in (4,8,16):
-			for r in (1,2,3):
-				y = np.zeros((1, P))
-				y[0][0] = yc
-				y[0][1:P/2] = yc - r
-				y[0][P/2] = yc
-				y[0][P/2+1:] = yc + r
-				"""de P-1 para 0"""
-				for i in range(P-1,-1,-1):
-					if(y[0][i]!=lbp.calculates_y_coordinate(yc, r, i, P)):
-						print " ",yc,r,i,P
-					self.assertEqual(y[0][i], lbp.calculates_y_coordinate(yc, r, i+1, P))
+    ######################################
 
-	def test_calculates_binary(self):
-		#calculates_binary(self, matVals)
-		lbp = LBP()
-		matVals = np.zeros((1,4))
-		matVals[0,0] = 0
-		matVals[0,1] = 1
-		matVals[0,2] = 0
-		matVals[0,3] = 1
-		binNum = lbp.calculates_binary(matVals)
-		self.assertEqual(binNum, 5)
-		
-		matVals2 = np.zeros((1,4))
-		matVals2[0,0] = 1
-		matVals2[0,1] = 1
-		matVals2[0,2] = 1
-		matVals2[0,3] = 1
-		binNum2 = lbp.calculates_binary(matVals2)
-		self.assertEqual(binNum2, 15)
-		
+    def test_computeLBP(self):
+        import scipy
+        lbp = LBP()
+
+        imgName = 'testarAoRedor.png'
+        img1 = scipy.misc.imread(imgName, flatten=True)
+
+        matLbp = lbp.computeLBP(img1, 4, 1)
+
+        self.assertEqual(matLbp[0,0], -1)
+
+
+    def test_calculates_x_coordinate(self):
+        lbp = LBP()
+        xc = 2
+        r = 1
+        pixel = 1
+        P = 4
+        x1 = xc
+        x2 = xc + 1
+        x3 = xc
+        x4 = xc - 1
+        x_viz1 = lbp.calculates_x_coordinate(xc, r, 1, P)
+        x_viz2 = lbp.calculates_x_coordinate(xc, r, 4, P)
+        x_viz3 = lbp.calculates_x_coordinate(xc, r, 3, P)
+        x_viz4 = lbp.calculates_x_coordinate(xc, r, 2, P)
+
+        self.assertEqual(x1, x_viz1)
+        self.assertEqual(x2, x_viz2)
+        self.assertEqual(x3, x_viz3)
+        self.assertEqual(x4, x_viz4)
+
+    def testPAcimaDoLimite(self):
+        imgName = 'testarAoRedor.png'
+        img1 = scipy.misc.imread(imgName, flatten=True)
+        lbp = LBP()
+        self.assertRaises(lbp.LBPOutOfRangeError, computeLBP, img1, 16, 1)
+
+    """
+    def testAutoValueX(self):
+        lbp = LBP()
+        xc = 2
+        for P in (4,8,16):
+            for r in (1,2,3):
+                x = np.zeros((1, P))
+                x[0] = xc + r
+                x[0][(P/4):P*3/4] = xc - r
+                x[0][(P/4)] = xc
+                x[0][(P*3/4)] = xc
+                for i in range(P-1,-1,-1):
+                    if(x[0][i]!=lbp.calculates_x_coordinate(xc, r, i, P)):
+                        print " ",xc,r,i,P
+                    self.assertEqual(x[0][i], lbp.calculates_x_coordinate(xc, r, i+1, P))
+    """
+
+    def test_calculates_y_coordinate(self):
+        # calculates_y_coordinate(self, yc, r, p, P)
+        lbp = LBP()
+        yc = 2
+        r = 1
+        pixel = 1
+        P = 4
+
+        y1 = yc - 1
+        y2 = yc
+        y3 = yc + 1
+        y4 = yc
+
+        y_viz1 = lbp.calculates_y_coordinate(yc, r, 1, P)
+        y_viz2 = lbp.calculates_y_coordinate(yc, r, 4, P)
+        y_viz3 = lbp.calculates_y_coordinate(yc, r, 3, P)
+        y_viz4 = lbp.calculates_y_coordinate(yc, r, 2, P)
+
+        self.assertEqual(y1, y_viz1)
+        self.assertEqual(y2, y_viz2)
+        self.assertEqual(y3, y_viz3)
+        self.assertEqual(y4, y_viz4)
+    """
+    def testAutoValueY(self):
+        lbp = LBP()
+        yc = 2
+        for P in (4,8,16):
+            for r in (1,2,3):
+                y = np.zeros((1, P))
+                y[0][0] = yc
+                y[0][1:P/2] = yc - r
+                y[0][P/2] = yc
+                y[0][P/2+1:] = yc + r
+                ""de P-1 para 0""
+                for i in range(P-1,-1,-1):
+                    if(y[0][i]!=lbp.calculates_y_coordinate(yc, r, i, P)):
+                        print " ",yc,r,i,P
+                    self.assertEqual(y[0][i], lbp.calculates_y_coordinate(yc, r, i+1, P))
+    """
+    def test_calculates_binary(self):
+        #calculates_binary(self, matVals)
+        lbp = LBP()
+        matVals = np.zeros((1,4))
+        matVals[0,0] = 0
+        matVals[0,1] = 1
+        matVals[0,2] = 0
+        matVals[0,3] = 1
+        binNum = lbp.calculates_binary(matVals)
+        self.assertEqual(binNum, 5)
+
+        matVals2 = np.zeros((1,4))
+        matVals2[0,0] = 1
+        matVals2[0,1] = 1
+        matVals2[0,2] = 1
+        matVals2[0,3] = 1
+        binNum2 = lbp.calculates_binary(matVals2)
+        self.assertEqual(binNum2, 15)
+
 if __name__ == '__main__':
     unittest.main()
